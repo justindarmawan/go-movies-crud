bc54af62ff79ea8aa2149ac9255ec2a06c6e0f0e
justin.darmawan <jdarmawan.jd@gmail.com>
2023-02-24 13:57:43 +0700

    initial

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	//"encoding/json"
+	//"math/rand"
+	"net/http"
+	//"strconv"
+	//"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovies).Methods("GET")
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+
+	fmt.Printf("Starting server at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
